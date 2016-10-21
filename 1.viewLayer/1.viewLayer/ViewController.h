4b2e343774f05ea0abbef95471129783dacd9606
cainrun <350116542@qq.com>
2016-10-21 13:49:08 +0800

    添加查看图层的工程
    添加查看图层的工程

---
+++ 1.viewLayer/1.viewLayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1.viewLayer
+//
+//  Created by Cain Luo on 21/10/16.
+//  Copyright © 2016年 Cain Luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 1.viewLayer/1.viewLayer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  1.viewLayer
+//
+//  Created by Cain Luo on 21/10/16.
+//  Copyright © 2016年 Cain Luo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
