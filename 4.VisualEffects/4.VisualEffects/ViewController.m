be7b5f56b56ffcd8976d037b8c6179c8589861cb
cainrun <350116542@qq.com>
2016-10-28 22:01:31 +0800

    添加图片以及示意图
    添加图片以及示意图

--- 4.VisualEffects/4.VisualEffects/ViewController.m
+++ 4.VisualEffects/4.VisualEffects/ViewController.m
@@ -20,7 +20,10 @@
 //    [self layerRoundedCorners];
 //    [self layerBorders];
 //    [self layerDropShadows];
-    [self layerShadowClipping];
+//    [self layerShadowClipping];
+//    [self layerShadowPath];
+//    [self layerMasking];
+    [self layerScalingFilters];
 }
 
 /**
@@ -95,6 +98,76 @@
     [shadowView addSubview:contentView];
     
     [self.view addSubview:shadowView];
+}
+
+- (void)layerShadowPath {
+    
+    UIView *shadowViewOne = [[UIView alloc] initWithFrame:CGRectMake(50, 50, 100, 100)];
+    UIView *shadowViewTwo = [[UIView alloc] initWithFrame:CGRectMake(50, 250, 100, 100)];
+
+    shadowViewOne.layer.shadowOpacity = 0.5f;
+    shadowViewTwo.layer.shadowOpacity = 0.5f;
+    
+    CGMutablePathRef squarePath = CGPathCreateMutable();
+    CGPathAddRect(squarePath, NULL, shadowViewOne.bounds);
+    shadowViewOne.layer.shadowPath = squarePath;
+    
+    CGPathRelease(squarePath);
+    
+    CGMutablePathRef circlePath = CGPathCreateMutable();
+    CGPathAddEllipseInRect(circlePath, NULL, shadowViewTwo.bounds);
+    shadowViewTwo.layer.shadowPath = circlePath;
+    
+    CGPathRelease(circlePath);
+
+    [self.view addSubview:shadowViewOne];
+    [self.view addSubview:shadowViewTwo];
+}
+
+/**
+ *  Layer Masking
+ */
+- (void)layerMasking {
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    
+    imageView.image = [UIImage imageNamed:@"arrow"];
+    
+    CALayer *maskLayer = [CALayer layer];
+    UIImage *maskImage = [UIImage imageNamed:@"star"];
+    
+    maskLayer.frame = imageView.bounds;
+    maskLayer.contents = (__bridge id _Nullable)(maskImage.CGImage);
+    
+    imageView.layer.mask = maskLayer;
+    
+    [self.view addSubview:imageView];
+}
+
+/**
+ *  Scaling Filters
+ */
+- (void)layerScalingFilters {
+    
+    UIImageView *imageViewOne = [[UIImageView alloc] initWithFrame:CGRectMake(0, 100, self.view.bounds.size.width / 2, 200)];
+    UIImageView *imageViewTwo = [[UIImageView alloc] initWithFrame:CGRectMake(self.view.bounds.size.width / 2, 100, self.view.bounds.size.width / 2, 200)];
+    UIImageView *imageViewThree = [[UIImageView alloc] initWithFrame:CGRectMake(0, 300, self.view.bounds.size.width / 2, 200)];
+    
+    imageViewOne.image = [UIImage imageNamed:@"expression"];
+    imageViewTwo.image = [UIImage imageNamed:@"expression"];
+    imageViewThree.image = [UIImage imageNamed:@"expression"];
+
+    imageViewOne.layer.magnificationFilter = kCAFilterNearest;
+    imageViewTwo.layer.magnificationFilter = kCAFilterLinear;
+    imageViewThree.layer.magnificationFilter = kCAFilterTrilinear;
+
+    imageViewOne.layer.minificationFilter = kCAFilterNearest;
+    imageViewTwo.layer.minificationFilter = kCAFilterLinear;
+    imageViewThree.layer.minificationFilter = kCAFilterTrilinear;
+    
+    [self.view addSubview:imageViewOne];
+    [self.view addSubview:imageViewTwo];
+    [self.view addSubview:imageViewThree];
 }
 
 @end
