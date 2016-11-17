e74dcf7c7152a97eb731313f5b3b6cf1eb9a9f84
cainrun <350116542@qq.com>
2016-11-18 02:05:50 +0800

    添加图片
    添加图片

--- 6.SpecializedLayers/6.SpecializedLayers/ViewController.m
+++ 6.SpecializedLayers/6.SpecializedLayers/ViewController.m
@@ -17,9 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self createPath];
+//    [self createPath];
+    [self viewRoundedCorners];
 }
 
+#pragma mark - CAShapeLayer
 - (void)createPath {
     
     UIBezierPath *path = [[UIBezierPath alloc] init];
@@ -28,8 +30,8 @@
     [path addQuadCurveToPoint:CGPointMake(100, 500)
                  controlPoint:CGPointMake(250, 600)];
     
-    //create shape layer
     CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+    
     shapeLayer.strokeColor = [UIColor blueColor].CGColor;
     shapeLayer.fillColor = [UIColor clearColor].CGColor;
     shapeLayer.lineWidth = 10;
@@ -37,7 +39,24 @@
     shapeLayer.lineCap = kCALineCapRound;
     shapeLayer.path = path.CGPath;
     
-    //add it to our view
+    [self.view.layer addSublayer:shapeLayer];
+}
+
+- (void)viewRoundedCorners {
+    
+    CGRect rect = CGRectMake(130, 130, 100, 100);
+    CGSize radii = CGSizeMake(10, 10);
+    
+    UIRectCorner corners = UIRectCornerTopRight | UIRectCornerBottomRight | UIRectCornerBottomLeft | UIRectCornerTopLeft;
+    
+    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:rect
+                                               byRoundingCorners:corners
+                                                     cornerRadii:radii];
+    
+    CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+    
+    shapeLayer.path = path.CGPath;
+    
     [self.view.layer addSublayer:shapeLayer];
 }
 
