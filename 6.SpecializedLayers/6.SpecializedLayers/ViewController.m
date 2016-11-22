87627a09ee90bcd9860f88df8f3564b567b8ca02
cainrun <350116542@qq.com>
2016-11-23 01:47:40 +0800

    添加图片
    添加图片

--- 6.SpecializedLayers/6.SpecializedLayers/ViewController.m
+++ 6.SpecializedLayers/6.SpecializedLayers/ViewController.m
@@ -23,7 +23,9 @@
 //    [self viewRoundedCorners];
 //    [self catextLayer];
 //    [self attributedString];
-    [self createCLLabel];
+//    [self createCLLabel];
+//    [self transformLayer];
+    [self gradientLayer];
 }
 
 #pragma mark - CAShapeLayer
@@ -148,6 +150,119 @@
     label.textColor = [UIColor blackColor];
     
     [self.view addSubview:label];
+}
+
+#pragma mark - CATransformLayer
+- (void)transformLayer {
+    
+    self.view.backgroundColor = [UIColor grayColor];
+    
+    CATransform3D transform3DOne = CATransform3DIdentity;
+    
+    transform3DOne.m34 = -1.0 / 500.0;
+    
+    self.view.layer.sublayerTransform = transform3DOne;
+    
+    CATransform3D transform3DTwo = CATransform3DIdentity;
+    
+    transform3DTwo = CATransform3DTranslate(transform3DTwo, -100, 0, 0);
+    
+    CALayer *cubeOne = [self cubeWithTransform:transform3DTwo];
+    
+    [self.view.layer addSublayer:cubeOne];
+    
+    CATransform3D transform3DThree = CATransform3DIdentity;
+    
+    transform3DThree = CATransform3DTranslate(transform3DThree, 100, 0, 0);
+    transform3DThree = CATransform3DRotate(transform3DThree, -M_PI_4, 1, 0, 0);
+    transform3DThree = CATransform3DRotate(transform3DThree, -M_PI_4, 0, 1, 0);
+    
+    CALayer *cubeTwo = [self cubeWithTransform:transform3DThree];
+    
+    [self.view.layer addSublayer:cubeTwo];
+}
+
+- (CALayer *)layerWithTransform:(CATransform3D)transform {
+    
+    CALayer *layer = [CALayer layer];
+    
+    layer.frame = CGRectMake(-50, -50, 100, 100);
+    
+    CGFloat red = (rand() / (double)INT_MAX);
+    CGFloat green = (100000 / (double)INT_MAX);
+    CGFloat blue = (rand() / (double)INT_MAX);
+    
+    layer.backgroundColor = [UIColor colorWithRed:red
+                                            green:green
+                                             blue:blue
+                                            alpha:1.0f].CGColor;
+    layer.transform = transform;
+    
+    return layer;
+}
+
+- (CALayer *)cubeWithTransform:(CATransform3D)transform {
+    
+    // cube
+    CATransformLayer *cube = [CATransformLayer layer];
+    
+    // layer one
+    CATransform3D transform3D = CATransform3DMakeTranslation(0, 0, 50);
+    [cube addSublayer:[self layerWithTransform:transform3D]];
+    
+    // layer two
+    transform3D = CATransform3DMakeTranslation(50, 0, 0);
+    transform3D = CATransform3DRotate(transform3D, M_PI_2, 0, 1, 0);
+    [cube addSublayer:[self layerWithTransform:transform3D]];
+    
+    // layer three
+    transform3D = CATransform3DMakeTranslation(0, -50, 0);
+    transform3D = CATransform3DRotate(transform3D, M_PI_2, 1, 0, 0);
+    [cube addSublayer:[self layerWithTransform:transform3D]];
+
+    // layer five
+    transform3D = CATransform3DMakeTranslation(-50, 0, 0);
+    transform3D = CATransform3DRotate(transform3D, -M_PI_2, 0, 1, 0);
+    [cube addSublayer:[self layerWithTransform:transform3D]];
+
+    // layer six
+    transform3D = CATransform3DMakeTranslation(0, 0, -50);
+    transform3D = CATransform3DRotate(transform3D, M_PI, 0, 1, 0);
+    [cube addSublayer:[self layerWithTransform:transform3D]];
+
+    CGSize containerSize = self.view.bounds.size;
+    
+    cube.position = CGPointMake(containerSize.width / 2.0,
+                                containerSize.height / 2.0);
+    
+    cube.transform = transform;
+    
+    return cube;
+}
+
+#pragma mark - CAGradientLayer
+- (void)gradientLayer {
+    
+    UIView *view = [[UIView alloc] init];
+    
+    view.bounds = CGRectMake(0, 0, 200, 200);
+    view.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2);
+    
+    CAGradientLayer *gradientLayer = [CAGradientLayer layer];
+    gradientLayer.frame = view.bounds;
+    
+    // 设置渐变的颜色, 理论上来讲是无限添加的
+    gradientLayer.colors = @[(__bridge id)[UIColor redColor].CGColor,
+                             (__bridge id)[UIColor greenColor].CGColor];
+    
+    gradientLayer.startPoint = CGPointMake(0, 0); // 开始渐变的点
+    gradientLayer.endPoint = CGPointMake(1, 1); // 结束渐变的点
+    
+    gradientLayer.locations = @[@0.0, @0.2]; // 设置渐变的区域
+    
+    [view.layer addSublayer:gradientLayer];
+    
+    [self.view addSubview:view];
 }
 
 @end
